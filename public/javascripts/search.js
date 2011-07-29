8fe022d0c7e9634f8108388e3c5d06bf54e6f1f7
Amit Ambardekar <amitamb@gmail.com>
2011-07-30 00:54:58 +0530

    Initial commit for search functionality 2

---
+++ public/javascripts/search.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+	
+	var config = {
+		type		: 'web',
+		append		: false,
+		perPage		: 8,			// A maximum of 8 is allowed by Google
+		page		: 0				// The start page
+	}
+  
+  $("#search").click(function(){
+    if ( config.term = $("#status").val() ){
+      googleSearch();
+    }
+		return false;
+  });
+  
+  $(document).click(function(){
+    if($('#main-dropdown').hasClass('show')) {
+        $("#main-dropdown").toggleClass('hide show');
+    }
+  });
+  
+  $("#main-dropdown").click(function(){
+    event.stopPropagation();
+  });
+	
+	
+	function googleSearch(settings){
+		
+		// If no parameters are supplied to the function,
+		// it takes its defaults from the config object above:
+		
+		settings = $.extend({},config,settings);
+		settings.term = settings.term;
+		
+		// URL of Google's AJAX search API
+		var apiURL = 'http://ajax.googleapis.com/ajax/services/search/'+settings.type+'?v=1.0&callback=?';
+		var resultsDiv = $('#resultsDiv');
+		
+		$.getJSON(apiURL,{q:settings.term,rsz:settings.perPage,start:settings.page*settings.perPage},function(r){
+			
+			var results = r.responseData.results;
+
+      var dropdown = $("#dropdown");
+
+      dropdown.html("");
+      
+      $("#main-dropdown").toggleClass('show hide');
+
+      if (results.length){
+        for (var i=0, len = results.length; i < len ; i++){
+          var r = results[i];
+
+          var new_item = $("<li><a href='"+ r.unescapedUrl +"'>" + r.visibleUrl + "</a></li>")
+          new_item.children("a").click(function(){
+            var url = $(this).attr("href");
+            $("#main-dropdown").toggleClass('hide show');
+            $("#status").val(url);
+            $("#status").change();
+            return false;
+          });
+          new_item.appendTo(dropdown);
+        }
+      }
+
+		});
+	}
+
+});
